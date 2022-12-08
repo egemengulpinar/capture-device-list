62d3b8461218aa2563682f5abcbd39154aee74d3
Egemen Gulpinar <egemengulpinar@gmail.com>
2022-12-09 02:06:21 +0300

    fix argument parser issues

--- capture-devices.py
+++ capture-devices.py
@@ -15,10 +15,10 @@ from utils import load_utils
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-list', '-l', nargs='?' ,type=str, help='Print the list of devices.')
-parser.add_argument('-save', '-s', nargs='?', type=str, help='Save the results to a file.')
-parser.add_argument('-audio', '-a', nargs='?', type=str, help='Select only aud.io devices.')
-parser.add_argument('-video', '-v', nargs='?', ype=str, help='Select only video devices.')
+parser.add_argument('-list', '-l', action='store_true' , default =True , help='Print the list of all devices.')
+parser.add_argument('-audio', '-a', action='store_true' ,  help='Select only audio devices.')
+parser.add_argument('-video', '-v', action='store_true', help='Select only video devices.')
+parser.add_argument('-save', '-s', action='store_true' ,  help='Save the results to a file.')
 args = parser.parse_args()
 
 
@@ -31,20 +31,26 @@ proc = subprocess.Popen([f'{ffmpeg_path}', '-stats', '-hide_banner','-list_devic
 stdout, stderr = proc.communicate()
 json_object = json.dumps(stderr.decode("UTF-8"))
 json_object = json.loads(json_object)
-if args.audio != None or args.video != None:
-    only_video_json_object = str([x for x in json_object.split("\n") if x.__contains__(f"({args.audio}, {args.video})")])    
-else:
-    only_video_json_object = str([x for x in json_object.split("\n") if x.__contains__("(audio, video)")])
+
+dict_args= {
+    
+    0: "(audio, video)",
+    1: "(audio)",
+    2: "(video)",
+}
+
+contain_object = dict_args[[x for x in [args.a1,args.a2,args.a3]].index(True)]
+only_video_json_object = str([x for x in json_object.split("\n") if x.__contains__(contain_object)])
 re_json_object = re.findall(r'"([^"]*)"', only_video_json_object)
 res = [x for x in re_json_object if not x.__contains__("@device")]
-if res == []:
+if res == [] and args.audio == False:
     only_video_json_object = str([x for x in json_object.split("\n") if x.__contains__("(video)")])
     re_json_object = re.findall(r'"([^"]*)"', only_video_json_object)
     res = [x for x in re_json_object if not x.__contains__("@device")]
 
-if args.list != None:
+if args.list != False:
     print(res)
-elif args.save !=None:
+elif args.save !=False:
     ###write the results to a file.
     with open('devices.txt',"w", encoding="utf-8") as output:
         output.write("Device List" + "\n")
