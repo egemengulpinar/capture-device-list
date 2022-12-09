f22367e4e7bcb5fbf4c8cf1551d3803383397621
Egemen Gulpinar <hegulpin@bradford.ac.uk>
2022-12-09 03:14:28 +0300

    optimized algorithm

--- capture-devices.py
+++ capture-devices.py
@@ -15,9 +15,12 @@ from utils import load_utils
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-list', '-l', action='store_true' , default =True , help='Print the list of all devices.')
+
 parser.add_argument('-audio', '-a', action='store_true' ,  help='Select only audio devices.')
 parser.add_argument('-video', '-v', action='store_true', help='Select only video devices.')
+parser.add_argument('-audio_video', '-av', action='store_true', help='Select only video devices.')
+parser.add_argument('-alternative', '-alt', action='store_true', help='Show alternative names.')
+parser.add_argument('-list_all', '-l', action='store_true' , default =True , help='Print the list of all devices.')
 parser.add_argument('-save', '-s', action='store_true' ,  help='Save the results to a file.')
 args = parser.parse_args()
 
@@ -34,27 +37,41 @@ json_object = json.loads(json_object)
 
 dict_args= {
     
-    0: "(audio, video)",
-    1: "(audio)",
-    2: "(video)",
+    
+    0: "(audio)",
+    1: "(video)",
+    2: "(audio, video)",
+    3: ""
 }
+print([x for x in [args.audio,args.video,args.audio_video,args.list_all]])
+contain_object = dict_args[[x for x in [args.audio,args.video,args.audio_video,args.list_all]].index(True)]
 
-contain_object = dict_args[[x for x in [args.a1,args.a2,args.a3]].index(True)]
-only_video_json_object = str([x for x in json_object.split("\n") if x.__contains__(contain_object)])
-re_json_object = re.findall(r'"([^"]*)"', only_video_json_object)
-res = [x for x in re_json_object if not x.__contains__("@device")]
-if res == [] and args.audio == False:
-    only_video_json_object = str([x for x in json_object.split("\n") if x.__contains__("(video)")])
-    re_json_object = re.findall(r'"([^"]*)"', only_video_json_object)
-    res = [x for x in re_json_object if not x.__contains__("@device")]
+print(contain_object)
 
-if args.list != False:
-    print(res)
-elif args.save !=False:
+for x in json_object.split("\n"):
+    try:
+        if x.__contains__(f"{contain_object}"):
+            print("-->", re.findall(r'"([^"]*)"', x )[0].__contains__("@device"))
+            if re.findall(r'"([^"]*)"', x )[0].__contains__("@device") == False:
+                only_video_json_object.append("DEVICE NAME : " + re.findall(r'"([^"]*)"', x )[0] )
+                cont = True
+        if cont == True: 
+            if args.alternative == True and re.findall(r'"([^"]*)"', x )[0].__contains__("@device") == True:
+                only_video_json_object.append("ALTERNATIVE NAME : " +  re.findall(r'"([^"]*)"', x )[0] + "\n")
+                cont = False
+    except:
+        continue
+#re_json_object = re.findall(r'"([^"]*)"', str(only_video_json_object))
+#print(only_video_json_object)
+
+if args.list_all != False:
+    print(only_video_json_object)
+if args.save !=False:
     ###write the results to a file.
     with open('devices.txt',"w", encoding="utf-8") as output:
-        output.write("Device List" + "\n")
-        for line in res:
+        output.write("###############################Device List##################################" + "\n")
+        output.write("----------------------------------------------------------------------------" + "\n")
+        for line in only_video_json_object:
             output.write(line + "\n")
     ###or you can print the results.
 else:
